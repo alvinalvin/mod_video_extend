a1672b3531210beed1378e258f83d1e094cb458b
Alvin Gil Saldaña <alvin_gil@hotmail.com>
2023-09-10 20:04:21 -0500

    Update mod_video_extend.php

--- mod_video_extend.php
+++ mod_video_extend.php
@@ -7,6 +7,29 @@
 defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 $doc = JFactory::getDocument();
+//recibiendo parametros
+$txtmarker1     = $params->get("txto1");
+$txtmarker2     = $params->get("txto2");
+$txtmarker3     = $params->get("txto3");
+$txtmarker4     = $params->get("txto4");
+$widthvideo     = $params->get("anchovideo");
+$heightvideo    = $params->get("largovideo");
+$widthlogo      = $params->get("anchologo");
+$heightlogo     = $params->get("largologo");
+$time1          = $params->get("tiempo1");
+$time2          = $params->get("tiempo2");
+$time3          = $params->get("tiempo3");
+$time5          = $params->get("tiempo5");
+$url            = $params->get("urllogo");
+$logo           = $params->get("logo");
+$video          = $params->get("video");
+$poster         = $params->get("poster");
+$muted          = $params->get("Muted");
+$autoplay       = $params->get("autoplay");
+$loop           = $params->get("loop");
+$preload        = $params->get("preload");
+$id             = $params->get("id");
+$video_youtube  = $params->get("urlyoutube");
 // Include jquery
 $doc->addScript(JURI::root()."modules/mod_video_extend/assets/js/jquery.video-extend.js");
 require JModuleHelper::getLayoutPath('mod_video_extend', $params->get('layout', 'default'));
